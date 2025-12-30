41224b1f31e645b7b295d07112e98b3bcc9d3b55
CardboardPL <mrplwaschilling@gmail.com>
2025-12-30 23:12:09 +0800

    feat(TaskStorage): Hydrate taskTree from localStorage
    Add #getFromStorage() to hydrate taskTree from localStorage.

--- src/modules/task-manager/TaskStorage.js
+++ src/modules/task-manager/TaskStorage.js
@@ -1,3 +1,5 @@
+import { Node } from './../../data-structures/LinkedList.js';
+import { Queue } from './../../data-structures/Queue.js';
 import { Tree } from './../../data-structures/Tree.js';
 
 export class TaskStorage {
@@ -15,6 +17,54 @@ export class TaskStorage {
         }
 
         this.#storageId = storageId;
-        this.#taskTree = new Tree();
+        this.#taskTree = new Tree(null, false);
+        this.#getFromStorage();
+    }
+
+    #getFromStorage() {
+        /* Structure of Stored Data
+            [{
+                globalId
+                taskData: {...}
+                },
+                children: [...]
+            }] 
+        */
+        // Retrieve Data From Storage
+        const rawData = JSON.parse(localStorage.getItem(this.#storageId));
+
+        // Verify if it's valid
+        if (!rawData) {
+            return;
+        } else if (!Array.isArray(rawData)) {
+            throw new Error('Invalid Storage Location: storage location stores the wrong data type.');
+        }
+
+        // Represent the raw data in the tree
+        const queue = new Queue();
+        if (rawData.length > 0) {
+            queue.enqueue(new Node(null, null, {
+                parentId: null,
+                data: rawData[0]
+            }));
+        }
+
+        while (queue.queueSize()) {
+            const { parentId, data } = queue.dequeue().data;
+            const currNodeParent = this.#taskTree.retrieveNode(parentId);
+            const { globalId, taskData, children } = data;
+
+            if ((!globalId && globalId !== 0) || !taskData || !Array.isArray(children)) {
+                throw new Error('Invalid Task Data');
+            }
+            this.#taskTree.appendChild(currNodeParent, taskData, globalId);
+
+            for (const child of children) {
+                queue.enqueue(new Node(null, null, {
+                    parentId: globalId,
+                    data: child
+                }));
+            }
+        }
     }
 }
