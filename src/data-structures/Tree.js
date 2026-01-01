3ba8f3d645852d9c4a2e84b6fd59729adf95c932
CardboardPL <mrplwaschilling@gmail.com>
2026-01-01 19:57:54 +0800

    refactor(Tree): enforce ID-based acccess and strengthen abstractions
    - Made #retrieveNode private to prevent external mutations.
    - insertParentAbove & appendChild now use node IDs instead of raw nodes.
    - Added retrieval helpers for node data, parent ID, and children IDs.
    - deleteSubtree and updateNodeData now rely on private retrieval for safety.

--- src/data-structures/Tree.js
+++ src/data-structures/Tree.js
@@ -55,69 +55,115 @@ export class Tree {
         return newNode;
     }
 
-    insertParentAbove(node, data, id) {
-        if (this.root && !node) {
+    #retrieveNode(nodeId) {
+        return this.#map.get(nodeId);
+    }
+
+    #removeNodesFromMap(startNode) {
+        const toProcess = new Queue();
+        this.#map.delete(startNode.data.id);
+        toProcess.enqueue(startNode);
+        
+        while (toProcess.queueSize()) {
+            for (const child of toProcess.dequeue().data.data.children) {
+                this.#map.delete(child.data.id);
+                toProcess.enqueue(child);
+            }
+        }
+    }
+
+    // The id parameter for insertParentAbove and appendChild allows the user to pass in custom ids when id generation is disabled.
+    insertParentAbove(descendantNodeId, data, id) {
+        if (this.root && !descendantNodeId) {
             throw new Error('Aborted Insert Node Process: Cannot insert before a null node when the tree already has a root.');
-        } else if (this.root === node) {
+        } else if (this.root.data.id === descendantNodeId) {
             throw new Error('Aborted Insert Node Process: Cannot insert before the root node.');
         }
 
-        if (node) {
-            if (!node.data || (!node.data.id && node.data.id !== 0)) throw new Error('Aborted Insert Node Process: Passed an invalid node');
-            if (!this.retrieveNode(node.data.id)) throw new Error('Aborted Insert Node Process: Node doesn\'t exist in the current tree');
-        }
+        const descendantNode =  this.#retrieveNode(descendantNodeId);
+        if (descendantNodeId != null && !descendantNode) throw new Error('Aborted Insert Node Process: Descendant Node doesn\'t exist in the current tree');
 
         let mapId = this.#generateId(id);
         const newNode = this.#createNewNode(mapId, data);
-        if (!this.root && node == null) {
-            this.root = newNode;   
+        if (!this.root && descendantNode == null) {
+            this.root = newNode;
         } else {
             // Node -> Node.data (TreeNode) ->  TreeNode.data
 
             // Make the parent of the original node the parent of the new node
-            newNode.data.data.parent = node.data.data.parent;
+            newNode.data.data.parent = descendantNode.data.data.parent;
 
             // Replace the original node in the children list of the parent node
-            node.data.data.parent.data.data.children.insertNode(node.prev, node.next, newNode);
+            descendantNode.data.data.parent.data.data.children.insertNode(descendantNode.prev, descendantNode.next, newNode);
 
             // Reset original node connections
-            node.prev = null;
-            node.next = null;
+            descendantNode.prev = null;
+            descendantNode.next = null;
 
             // Add the original node to the children list of the new node
-            newNode.data.data.children.appendNode(node);
+            newNode.data.data.children.appendNode(descendantNode);
             
             // Make the parent node of the original node the new node
-            node.data.data.parent = newNode;
+            descendantNode.data.data.parent = newNode;
         }
     
         return mapId;
     }
 
-    appendChild(parent, data, id) {
-        if (this.root && parent == null) {
+    appendChild(parentNodeId, data, id) {
+        if (this.root && parentNodeId == null) {
             throw new Error('Aborted Append Child Process: You can only create one root node');
         }
 
-        if (parent) {
-            if (!parent.data || (!parent.data.id && parent.data.id !== 0)) throw new Error('Aborted Append Child Process: Passed an invalid parent');
-            if (!this.retrieveNode(parent.data.id)) throw new Error('Aborted Append Child Process: Node doesn\'t exist in the current tree');
-        }
+        const parentNode = this.#retrieveNode(parentNodeId);
+        if (parentNodeId != null && !parentNode) throw new Error('Aborted Append Child Process: Parent Node doesn\'t exist in the current tree')
         
         const mapId = this.#generateId(id);
         const newNode = this.#createNewNode(mapId, data);
-        if (!this.root && parent == null) {
+        if (!this.root && parentNode == null) {
             this.root = newNode;
             return mapId;
         }
 
         // Node -> Node.data (TreeNode) -> TreeNode.data
-        parent.data.data.children.appendNode(newNode);
+        parentNode.data.data.children.appendNode(newNode);
         return mapId;
     }
 
+    retrieveNodeData(nodeId) {
+        const node = this.#retrieveNode(nodeId);
+        if (!node) throw new Error('Aborted Parent Node ID Retrieval Process: Node doesn\'t exist in the tree');
+        return node.data.data;
+    }
+
+    retrieveParentNodeId(nodeId) {
+        const node = this.#retrieveNode(nodeId);
+        if (!node) throw new Error('Aborted Parent Node ID Retrieval Process: Node doesn\'t exist in the tree');
+        const parentNode = node.data.data.parent;
+        return parentNode ? parentNode.data.id : null;
+    }
+
+    retrieveChildrenNodeIds(nodeId) {
+        const node = this.#retrieveNode(nodeId);
+        if (!node) throw new Error('Aborted Parent Node ID Retrieval Process: Node doesn\'t exist in the tree');
+        const idArray = [];
+
+        for (const node of node.data.data.children) {
+            idArray.push(node.data.id);
+        }
+
+        return idArray;
+    }
+
+    updateNodeData(nodeId, data) {
+        const node = this.#retrieveNode(nodeId);
+        if (!node) throw new Error('Aborted Node Update Process: Node doesn\'t exist in the tree.');
+
+        node.data.data.data = data;
+    }
+
     deleteSubtree(nodeId) {
-        const node = this.retrieveNode(nodeId);
+        const node = this.#retrieveNode(nodeId);
         if (!node) throw new Error('Aborted Subtree Deletion Process: Node doesn\'t exist in the tree.');
 
         if (this.root === node) {
@@ -135,30 +181,6 @@ export class Tree {
         }
 
         this.#removeNodesFromMap(node);
-    }
-
-    #removeNodesFromMap(startNode) {
-        const toProcess = new Queue();
-        this.#map.delete(startNode.data.id);
-        toProcess.enqueue(startNode);
-        
-        while (toProcess.queueSize()) {
-            for (const child of toProcess.dequeue().data.data.children) {
-                this.#map.delete(child.data.id);
-                toProcess.enqueue(child);
-            }
-        }
-    }
-
-    retrieveNode(nodeId) {
-        return this.#map.get(nodeId);
-    }
-
-    updateNodeData(nodeId, data) {
-        const node = this.retrieveNode(nodeId);
-        if (!node) throw new Error('Aborted Node Update Process: Node doesn\'t exist in the tree.');
-
-        node.data.data.data = data;
     }
 }
 
