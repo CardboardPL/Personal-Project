73de223b4975a726b3120c9d74c7967e387238df
CardboardPL <mrplwaschilling@gmail.com>
2026-01-18 20:38:21 +0800

    refactor(Router): turn the class into a skeleton for overhauls

--- src/modules/router/Router.js
+++ src/modules/router/Router.js
@@ -5,46 +5,31 @@ export class Router {
     #navTree;
     #renderer;
 
-    constructor(rootElem, rootHTML, fallbackHTML) {
-        this.#navTree = new RouterTree(rootHTML);
-        this.#renderer = new RouterRenderer(rootElem, this, fallbackHTML);
-        this.navigateTo(window.location.pathname);
-
-        window.addEventListener('popstate', (e) => {
-            if (!e.state.called) {
-                this.#renderer.renderPathMain(window.location.pathname);
-            }
-        });
+    constructor() {
+        
     }
 
-    addPath(segment, absParentNode, segmentHTML) {
-        return  this.#navTree.addChildSegment(absParentNode, segmentHTML, segment); 
+    addPath() {
+        
     }
 
-    findSegmentNode(absPath) {
-        return this.#navTree.findSegmentNode(absPath);
+    findSegmentNode() {
+        
     }
 
-    removePath(absPath) {
-        return this.#navTree.removePath(absPath);
+    removePath() {
+       
     }
 
-    getNodeContent(node) {
-        if (!node || !node.data || !node.data.data || !node.data.data.data) return null; 
-        return node.data.data.data;
+    getNodeContent() {
+        
     }
 
-    getSegmentHTML(absPath) {
-        const node = this.findSegmentNode(absPath);
-        const nodeContent = this.getNodeContent(node);
-        return nodeContent ? 
-            (nodeContent.data == null ? null : nodeContent.data) : 
-            null;
+    getSegmentHTML() {
+        
     }
 
-    navigateTo(absPath, data) {
-        const renderResult = this.#renderer.renderPathMain(absPath);
-        history.pushState(data, '', renderResult.absPath);
-        window.dispatchEvent(new PopStateEvent('popstate', { state: { called: true, data } }));
+    navigateTo() {
+        
     }
 }
