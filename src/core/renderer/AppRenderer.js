b3e1fe37758fef6723e6d10f676cd5dc534919f1
CardboardPL <mrplwaschilling@gmail.com>
2026-01-22 23:14:54 +0800

    feat(AppRenderer): add rootElem validation for safety

--- src/core/renderer/AppRenderer.js
+++ src/core/renderer/AppRenderer.js
@@ -2,8 +2,27 @@ class AppRenderer {
     #rootElem;
     
     constructor(rootElem) {
-        // Add Checks for Root Elem
+        this.#validateRootElem(rootElem);
         this.#rootElem = rootElem;
+    }
+
+    #validateRootElem(rootElem) {
+        if (!(rootElem instanceof HTMLElement)) throw new Error('rootElem must be an HTML element.');
+        
+        const forbiddenTags = [
+            // Doesn't make sense to render in these elements
+            'HTML', 'HEAD',
+
+            // Doesn't make sense to append HTML to a script or style tag
+            'STYLE', 'SCRIPT',
+
+            // Void tags can't have children
+            'AREA', 'BASE', 'BR', 'COL', 'EMBED', 
+            'HR', 'IMG', 'INPUT', 'LINK', 'META',
+            'PARAM', 'SOURCE', 'TRACK', 'WBR'
+        ]
+
+        if (forbiddenTags.includes(rootElem.tagName)) throw new Error('The provided rootElem is a forbidden tag.');
     }
 
     renderContainer(template) {
