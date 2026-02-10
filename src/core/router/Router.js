7aa22f77c5b62d9d2a2ee937a032fd362c6e7845
CardboardPL <mrplwaschilling@gmail.com>
2026-02-10 21:36:58 +0800

    feat(Router): add navigateTo() method and use history API

--- src/core/router/Router.js
+++ src/core/router/Router.js
@@ -11,6 +11,12 @@ export class Router {
             app: appRenderer
         };
         this.#navTree = new RouterTree(errorConfig);
+
+        window.addEventListener('popstate', (e) => {
+            this.navigateTo(window.location.pathname, false, e.state);
+        });
+
+        this.navigateTo(window.location.pathname);
     }
 
     addSegment(parentPath, segmentName, data) {
@@ -25,7 +31,26 @@ export class Router {
        this.#navTree.removeSegment(path);
     }
 
-    navigateTo() {
+    async navigateTo(path, addToHistory = false, entryData) {
+        let data;
         
+        try {
+            data = this.getSegmentData(path);
+        } catch(err) {
+            data = err.details;
+        }
+        
+        const { controller } = await import(data.js);
+        const packagedData = { data: {
+            html: data.html,
+            css: data.css,
+            context: data.context
+        }, controller }
+        
+        if (addToHistory) {
+            history.pushState(entryData, '', path);
+        }
+
+        this.#renderer.router.renderMain(packagedData, entryData);
     }
 }
