c0e85100a5b51edd509888ddbb262b712a91fd4e
CardboardPL <mrplwaschilling@gmail.com>
2026-02-11 19:53:56 +0800

    feat(core): add EventRegistry skeleton

---
+++ src/core/events/EventRegistry.js
@@ -0,0 +1,19 @@
+export class EventRegistry {
+    #listeners;
+
+    constructor() {
+        this.#listeners = new Map();
+    }
+
+    addListener(element, event, handler) {
+        
+    }
+
+    removeListener(element, event) {
+
+    }
+
+    clearAll() {
+        
+    }
+}
